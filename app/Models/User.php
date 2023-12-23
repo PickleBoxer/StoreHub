ad262924b391de25dd37ef869ba2d1e4e6a0e2aa
PickleBoxer <matic.vertacnik@icloud.com>
2023-12-23 20:56:32 +0100

    Add soft deletes to User model and create migration file
    This commit adds the `SoftDeletes` trait to the `User` model and creates a migration file to add the `softDeletes` column to the `users` table.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,13 @@ use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
 use Laravel\Jetstream\HasProfilePhoto;
 use Spatie\Permission\Traits\HasRoles;
-use Illuminate\Notifications\Notifiable;
-use BezhanSalleh\FilamentShield\Traits\HasPanelShield;
-use Filament\Models\Contracts\FilamentUser;
 use Filament\Models\Contracts\HasAvatar;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use BezhanSalleh\FilamentShield\Traits\HasPanelShield;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -26,6 +27,7 @@ class User extends Authenticatable implements FilamentUser, MustVerifyEmail, Has
     use TwoFactorAuthenticatable;
     use HasRoles;
     use HasPanelShield;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
