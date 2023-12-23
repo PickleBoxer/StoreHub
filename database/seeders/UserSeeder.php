fc7563ec9bae0ca4ea6e3a8233db9668e7a10baf
PickleBoxer <matic.vertacnik@icloud.com>
2023-12-23 18:15:55 +0100

    Add Shield and User seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => now(),
+        ]);
+
+        // Attach the super admin role to the admin user
+        $admin->assignRole(Role::whereName('super_admin')->firstOrFail());
+        // Create a personal team for the admin user for Jetstream
+        $this->createTeam($admin);
+    }
+
+    /**
+     * Create a personal team for the user.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    protected function createTeam(User $user)
+    {
+        $user->ownedTeams()->save(Team::forceCreate([
+            'user_id' => $user->id,
+            'name' => 'Personal',
+            'personal_team' => true,
+        ]));
+    }
+}
