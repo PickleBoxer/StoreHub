e9f23b4e0ff5dd6401b872fdf964ba2b5271ab02
PickleBoxer <matic.vertacnik@icloud.com>
2023-12-28 15:04:39 +0100

    Add PrestaShop webservice library dependency and test page

---
+++ resources/views/prestashop.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>CRUD Tutorial - Retrieve example</title>
+</head>
+<body>
+    <!-- Your HTML code goes here -->
+
+    @if (isset($resources))
+        <!-- Your HTML code that uses $resources -->
+    @endif
+</body>
+</html>
